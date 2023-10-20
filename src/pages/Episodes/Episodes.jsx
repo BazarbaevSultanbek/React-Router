06ec50179364b15b6912d60e5f882a7d2fb77e38
Sultanbek Bazarbaev <126026712+SultanbekBazarbaev@users.noreply.github.com>
2023-10-20 21:06:51 +0500

    Add files via upload

--- src/pages/Episodes/Episodes.jsx
+++ src/pages/Episodes/Episodes.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './epsiodes.scss'
 import {Link} from "react-router-dom";
 import { motion } from 'framer-motion';
@@ -24,30 +23,4 @@ function Episodes() {
         </motion.div>
     );
 }
-
-=======
-import React from 'react';
-import './epsiodes.scss'
-import {Link} from "react-router-dom";
-function Episodes() {
-    return (
-        <div className="Episodes">
-            <Link to={"/"}><div className="Episodes-back">back</div></Link>
-            <h2>Episodes</h2>
-            <div className="Episodes-all">
-                <div>Homer Simpson</div>
-                <div>Marge Simpson</div>
-                <div>Bart Simpson</div>
-                <div>Lisa Simpson</div>
-                <div>Maggie Simpson</div>
-                <div>Ned Flanders</div>
-                <div>Maude Flanders</div>
-                <div>Charles Montgomery Burns</div>
-                <div>Waylon Smithers</div>
-            </div>
-        </div>
-    );
-}
-
->>>>>>> db509e1aef323e56a00400905857459f53cc6bd5
 export default Episodes;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { motion } from 'framer-motion';
 import {Link} from "react-router-dom";
 import './main.scss'
@@ -18,22 +17,4 @@ function Main() {
     );
 }
 
-=======
-import React from 'react';
-import {Link} from "react-router-dom";
-import './main.scss'
-
-function Main() {
-    return (
-        <div className="Main">
-            <h2>The Simpsons</h2>
-            <div className="Main-pages">
-                <Link to={"/characters"}><div className="Main-pages-characters">Characters</div></Link>
-                <Link to={"/episodes"}><div className="Main-pages-episodes">Episodes</div></Link>
-            </div>
-        </div>
-    );
-}
-
->>>>>>> db509e1aef323e56a00400905857459f53cc6bd5
 export default Main;
